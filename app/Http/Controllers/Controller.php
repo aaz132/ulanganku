282a63840bb48fe8b72f2198e020fe37ac632526
aaz132 <revipermana3@gmail.com>
2020-09-01 13:27:47 +0700

    ada perubahan di controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,19 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function sendSuccess($data, $message = '', $code = 200) {
+        return response([
+          'success' => true,
+          'message' => $message,
+          'data' => $data
+        ], $code)->header('Content-Type', 'application/json');
+    }
+    public function sendError($error, $message, $code = 400) {
+        return response([
+            'success' => false,
+            'message' => $message,
+            'errors' => $error
+        ], $code)->header('Content-Type', 'application/json');
+    }
 }
